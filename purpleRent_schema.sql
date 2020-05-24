0987a148bfde4924238a02b1b76d57418fa1ba29
kilua <matteom0797@me.com>
2020-05-24 17:53:31 +0200

    .

--- purpleRent_schema.sql
+++ purpleRent_schema.sql
@@ -1,29 +1,49 @@
 create table Sede(
   codiceMnemonico character(5) primary key,
-  orarioApertura time,
-  orarioChiusura time,
-  telefono bigInt,
-  email varchar(30)
+  orarioApertura time not null,
+  orarioChiusura time not null,
+  telefono bigInt not null,
+  email varchar(30) not null
 );
 
 create table Dipendente(
   id int primary key,
-  nome varchar(12),
-  cognome varchar(12),
+  nome varchar(12) not null,
+  cognome varchar(12) not null,
   anniAnzianità int,
   sede character(5) not null,
-  foreign key (sede) references Sede(codiceMnemonico)
+  foreign key sede references Sede(codiceMnemonico)
 );
 
 create table ClienteNoleggio(
-  numDocumento,
-  nome,
-  cognome,
-  età,
-  datiBancari
-);
+  numDocumento varchar(10) primary key,
+  nome varchar(12) not null,
+  cognome varchar(12) not null,
+  età int not null,
+  datiBancari bigInt not null,
+  foreign key datiBancari references DatiBancari(contoCorrente)
+); *
 
 create table sedePreferita(
-  cliente int primary key
-  sede
+  cliente int primary key,
+  sede character(5),
+  foreign key cliente references ClienteNoleggio(numDocumento),
+  foreign key sede references Sede(codiceMnemonico)
+); *
+
+create table Prenotazione(
+  numeroPrenotazione int auto_increment primary key ,
+  orarioInizio dateTime,
+  orarioFine dateTime,
+  prezzo int,
+  sedeRitiro,
+  sedeRilascio,
+  cliente int,
+  carGroup
+  foreign key
+);
+
+create table CarGroup(
+  lettera,
+  prezzoGiornaliero
 );
