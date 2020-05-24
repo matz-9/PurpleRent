cf0d4c8f9ca09ae869ce7afedbf3c7dae22fd333
kilua <matteom0797@me.com>
2020-05-24 18:37:23 +0200

    update

---
+++ purpleRent_data.sql
@@ -0,0 +1,13 @@
+insert into Sede(codiceMnemonico,orarioApertura,orarioChiusura,telefono,email) values
+  ('61040','08:30','19:00','3791062597','pippolos@sede1.it'),
+  ('61031','08:30','18:30','3798000197','roma@sede2.it'),
+  ('41261','08:30','19:00','0863479810','math@sede3.com'));
+
+insert into Dipendente(id,nome,cognome,anniAnzianità,sede) values
+  ('1755368','Filippo','Sardi','20','61040'),
+  ('1718223','Mattia','Mariano','17','61040'),
+  ('1891110','Yegor','Kumiscavich','35','41261'));
+
+insert into ClienteNoleggio(numDocumento,nome,cognome,età,datiBancari) values
+  ('AP1017Y890','Vittorio','Terzetti','50','123000'),
+  ();
--- purpleRent_schema.sql
+++ purpleRent_schema.sql
@@ -22,28 +22,100 @@ create table ClienteNoleggio(
   età int not null,
   datiBancari bigInt not null,
   foreign key datiBancari references DatiBancari(contoCorrente)
-); *
+);
 
 create table sedePreferita(
   cliente int primary key,
   sede character(5),
   foreign key cliente references ClienteNoleggio(numDocumento),
   foreign key sede references Sede(codiceMnemonico)
-); *
+);
 
 create table Prenotazione(
-  numeroPrenotazione int auto_increment primary key ,
-  orarioInizio dateTime,
-  orarioFine dateTime,
-  prezzo int,
-  sedeRitiro,
-  sedeRilascio,
-  cliente int,
-  carGroup
-  foreign key
+  numeroPrenotazione int auto_increment primary key,
+  orarioInizio dateTime not null,
+  orarioFine dateTime not null,
+  prezzo int not null,
+  sedeRitiro character(5) not null,
+  sedeRilascio character(5) not null,
+  cliente int not null,
+  carGroup char not null,
+  foreign key sedeRitiro references Sede(codiceMnemonico),
+  foreign key sedeRilascio references Sede(codiceMnemonico),
+  foreign key cliente references ClienteNoleggio(numDocumento),
+  foreign key carGroup references CarGroup(lettera),
 );
 
 create table CarGroup(
-  lettera,
-  prezzoGiornaliero
+  lettera char,
+  prezzoGiornaliero int
+);
+
+create table LetteraNoleggio(
+  numeroLettera int auto_increment primary key,
+  kmPercorsi int not null,
+  tipo enum("aperta","chiusa") not null,
+  prenotazione int auto_increment not null,
+  datiBancari bigInt not null,
+  foreign key prenotazione references Prenotazione(numeroPrenotazione),
+  foreign key datiBancari references DatiBancari(contoCorrente)
+);
+
+create table Feedback(
+  noleggio int auto_increment primary key,
+  voto votoInt not null,
+  recensione text,
+  foreign key noleggio references LetteraNoleggio(numeroLettera)
+);
+
+--vincolo di dominio riferito al voto nel feedBack
+create domain votoInt as
+  integer check (value >= 0 and value <= 5);
+
+create table Indirizzo(
+  città varchar(20),
+  civico int,
+  via varchar (30),
+  cap character(5) not null,
+  primary key (città, civico, via)
+);
+
+create table indirizzoSede(
+  sede character(5) primary key,
+  città varchar(20),
+  civico int,
+  via varchar(30),
+  unique (città, civico, via)
+  foreign key sede references Sede(codiceMnemonico)
+);
+
+create table residenzaCliente(
+  cliente varchar(10) primary key,
+  città varchar(20),
+  civico int,
+  via varchar(30),
+  unique (città,civico,via),
+  foreign key cliente references ClienteNoleggio(numDocumento),
+  foreign key (città,civico,via) references Indirizzo(città,civico,via),
+);
+
+create table DatiBancari(
+  contoCorrente bigInt primary key,
+  circuito enum('visa', 'mastercard', 'american express', 'diner', 'bancoposta'),
+  nomeBanca varchar(15) not null
+);
+
+create table CasaAutomobilistica(
+  nome varchar(15) primary key,
+  narzione varchar(10) not null
+);
+
+create table AutovetturaNoleggiabile(
+  targa,
+  immatricolazione,
+  km,
+  colore,
+  disponibile,
+  carGroup,
+  casaAuto
 );
