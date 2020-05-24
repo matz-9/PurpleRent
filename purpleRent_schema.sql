d65ca9f9edf441c5da1dba439920ca5e56c87782
kilua <matteom0797@me.com>
2020-05-24 17:06:50 +0200

    aggiunta file data e creazione prime tables

--- purpleRent_schema.sql
+++ purpleRent_schema.sql
@@ -1,7 +1,25 @@
 create table Sede(
-  codiceMnemonico varchar(5) primary key,
-  orarioApertura time(1),
-  orarioChiusura time(1),
-  telefono
-  email
+  codiceMnemonico character(5) primary key,
+  orarioApertura time,
+  orarioChiusura time,
+  telefono bigInt,
+  email varchar(30)
+);
+
+create table Dipendente(
+  id int primary key,
+  nome varchar(12),
+  cognome varchar(12),
+  anniAnzianità int,
+  sede character(5) not null,
+  foreign key (sede) references Sede(codiceMnemonico)
+);
+
+create table ClienteNoleggio(
+  
+);
+
+create table sedePreferita(
+  cliente int primary key
+  sede
 );
