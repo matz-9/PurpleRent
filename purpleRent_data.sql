da04bf13b103f630294592533044da0958945e14
Kiluah <igor.provornyy@gmail.com>
2020-05-26 17:17:51 +0200

    data 2.0

--- purpleRent_data.sql
+++ purpleRent_data.sql
@@ -1,9 +1,8 @@
-
 --3 indirizzi
 insert into Sede(codiceMnemonico,orarioApertura,orarioChiusura,telefono,email) values
   ('61040', '08:30', '19:00', 3791062597, 'pippolos@purplerent.it'),
   ('61031', '08:30', '18:30', 3798000197, 'roma@purplerent.it'),
-  ('41261', '08:30', '19:00', 0863479810, 'math@purplrent.it');
+  ('41261', '08:30', '19:00', 0863479810, 'math@purplerent.it');
 
 insert into Dipendente(id,nome,cognome,anniAnzianità,sede) values
   (1755368, 'Filippo', 'Sardi',       20, '61040'),
@@ -28,26 +27,26 @@ insert into CarGroup(lettera,prezzoGiornaliero) values
 
 -- cliente + sede + fornitore + acquirente + officina = 7 + 3 + 4 + 4 + 2 = 20
 insert into Indirizzo(città,civico,via,cap) values
-  ('Roma',               '2',   'Via Ugo Bertossi',       '00159'), --ClienteNoleggio
-  ('Milano',             '5',   'Via Alessandro Manzoni', '35126'), --ClienteNoleggio
-  ('Roma',               '15',  'Via Regina Margherita',  '00198'), --ClienteNoleggio
-  ('Canistro Inferiore', '7',   'Via Largo Liri',         '67050'), --ClienteNoleggio
-  ('Roma',               '6',   'Via Mar Rosso',          '00121'), --ClienteNoleggio
-  ('Napoli',             '73',  'Via Napoli',             '00184'), --ClienteNoleggio
-  ('Firenze',            '6',   'Via Maremma Bucaiola',   '20199'), --ClienteNoleggio
-  ('Pisa',               '22',  'Via della Torre',        '45009'), --Acquirente
-  ('Roma',               '1',   'Via Tiburtina',          '00133'), --Acquirente
-  ('Genova',             '14',  'Via degli Abeti',        '04310'), --Acquirente
-  ('Bari',               '9',   'Via Cristian Cocco',     '11159'), --Acquirente
-  ('Milano',             '17',  'Piazza del Duomo',       '05409'),
-  ('Roma',               '113', 'Via Salaria',            '10231'),
-  ('Rieti',              '36',  'Via Virgola Mobile',     '01274');
-  ('Bologna',            '19',  'Via Guerrazzi',          '45009'),
-  ('Catanzaro',          '32',  'Via Guiuseppe Poeri',    '00133'),
-  ('Venezia',            '14',  'Calla Fenice',           '05310'),
-  ('Bari',               '9',   'Via Cristian Cocco',     '21359'),
-  ('Milano',             '1',   'Via Fieno',              '07419'),
-  ('Ventotene',          '24',  'Via Olivi',              '04020');
+  ('Roma',               '2',   'Via Ugo Bertossi',         '00159'), --ClienteNoleggio
+  ('Milano',             '5',   'Via Alessandro Manzoni',   '35126'), --ClienteNoleggio
+  ('Roma',               '15',  'Via Regina Margherita',    '00198'), --ClienteNoleggio
+  ('Canistro Inferiore', '7',   'Via Largo Liri',           '67050'), --ClienteNoleggio
+  ('Roma',               '6',   'Via Mar Rosso',            '00121'), --ClienteNoleggio
+  ('Napoli',             '73',  'Via Napoli',               '00184'), --ClienteNoleggio
+  ('Firenze',            '6',   'Via Maremma Bucaiola',     '20199'), --ClienteNoleggio
+  ('Pisa',               '22',  'Via della Torre',          '45009'), --Acquirente
+  ('Roma',               '1',   'Via Tiburtina',            '00133'), --Acquirente
+  ('Genova',             '14',  'Via degli Abeti',          '04310'), --Acquirente
+  ('Bari',               '9',   'Via Cristian Cocco',       '11159'), --Acquirente
+  ('Milano',             '17',  'Piazza del Duomo',         '05409'), --Officina
+  ('Roma',               '113', 'Via Salaria',              '10231'), --Officina
+  ('Rieti',              '36',  'Via Virgola Mobile',       '01274'),
+  ('Bologna',            '19',  'Via Guerrazzi',            '45009'),
+  ('Catanzaro',          '32',  'Via Guiuseppe Poeri',      '00133'),
+  ('Firenze',            '14',  'Viale Alessandro Guidoni', '05310'), --Sede
+  ('Roma',               '9',   'Via Cristian Cocco',       '21359'), --Sede
+  ('Milano',             '1',   'Via Fieno',                '07419'), --Sede
+  ('Ventotene',          '24',  'Via Olivi',                '04020');
 
 --cliente + fornitore + acquirente = 7 + 4 + 4 = 15
 insert into DatiBancari(contoCorrente,circuito,nomeBanca) values
@@ -72,11 +71,8 @@ insert into ClienteNoleggio(numDocumento,nome,cognome,età,datiBancari) values
   ('AP1017Y890', 'Vittorio', 'Terzetti',     50, '1233958372'),
   ('BO34590P70', 'Enzo',     'Pesce',        22, '3618304719'),
   ('T946510KP4', 'Marco',    'Marchi',       35, '4639281049'),
-  --('456QOP760Y', 'Filippo',  'Franceschini', 23, '4730184936'),
   ('78OPISL348', 'Guido',    'Piano',        63, '5930275641'),
-  --('56UR64FVOP', 'Davide',   'Maria',        32, '3649264281'),
   ('GRT780H12L', 'Harry',    'Foreman',      43, '9243856384'),
-  --('TRY79O012K', 'Guerrino', 'Di agostino',  56, '7692394913'),
   ('YR7I012KLU', 'Walter',   'Bishop',       45, '5629472036'),
   ('RJ51230KL7', 'Eric',     'Cartman',      21, '1947294759');
 
@@ -93,14 +89,14 @@ insert into sedePreferita(cliente,sede) values
   ('RJ51230KL7',  null);
 
 insert into Prenotazione(numeroPrenotazione,orarioInizio,orarioFine,prezzo,sedeDiRitiro,sedeDiRilascio,cliente,carGroup) values
-  (7001, '2018-01-14 09:00', '2018-01-18 09:00', 90.00,  '61031', '61031', 'AP1017Y890', 'E'),
-  (7002, '2018-08-22 10:17', '2018-08-23 10:17', 9.00,   '41261', '41261', 'AP1017Y890', 'A'),
+  (7001, '2020-05-22 09:00', '2020-06-28 09:00', 90.00,  '61031', '61031', 'AP1017Y890', 'E'),
+  (7002, '2020-08-27 10:17', '2020-08-28 10:17', 9.00,   '41261', '41261', 'AP1017Y890', 'A'),
   (7003, '2018-04-02 15:30', '2018-04-03 15:30', 14.00,  '61040', '41261', 'TRY79O012K', 'D'),
   (7004, '2019-02-18 11:00', '2019-02-20 11:00', 24.00,  '61040', '41261', '56UR64FVOP', 'C'),
   (7005, '2019-04-22 15:00', '2019-04-24 15:00', 18.00,  '61031', '61031', '456QOP760Y', 'A'),
   (7006, '2019-03-03 10:05', '2019-03-06 10:05', 54.00,  '41261', '41261', '456QOP760Y', 'G'),
   (7007, '2019-05-05 11:15', '2019-05-06 11:15', 55.00,  '41261', '61031', '456QOP760Y', 'L'),
-  (7008, '2019-04-05 12:15', '2019-04-07 12:15', 110.00, '41261', '61031', '456QOP760Y', 'L'),
+  (7008, '2019-04-05 12:15', '2019-04-07 12:15', 90.00,  '41261', '61031', '456QOP760Y', 'H'),
   (7009, '2019-06-25 17:30', '2019-06-26 17:30', 20.00,  '61031', '61031', 'GRT780H12L', 'J'),
   (7010, '2019-04-09 17:00', '2019-04-18 17:00', 81.00,  '61040', '61031', 'TRY79O012K', 'A'),
   (7011, '2020-01-22 08:30', '2020-01-30 08:30', 96.00,  '61040', '41261', 'TRY79O012K', 'C'),
@@ -109,8 +105,8 @@ insert into Prenotazione(numeroPrenotazione,orarioInizio,orarioFine,prezzo,sedeD
 insert into LetteraNoleggio(numeroLettera,kmPercorsi,tipo,prenotazione,datiBancari) values
   (00001, null, 'aperta', 7001, '3456175110'),
   (00002, null, 'aperta', 7002, '2121175432'),
-  (00003, null, 'aperta', 7003, '1996174444'),
-  (00004, null, 'aperta', 7004, '1331661919'),
+  (00003, 311,  'chiusa', 7003, '1996174444'),
+  (00004, 1190, 'chiusa', 7004, '1331661919'),
   (00005, 720,  'chiusa', 7005, '1203125891'),
   (00006, 1410, 'chiusa', 7006, '1506175891'),
   (00007, 2101, 'chiusa', 7007, '1506779391'),
@@ -131,9 +127,9 @@ insert into Feedback(noleggio,voto,recensione) values
   (00012, 5, 'ottima vettura, PurpleRent ha da sempre le lussuose più belle e veloci');
 
 insert into indirizzoSede(sede,città,civico,via) values
-  ('61040', 'Roma', ''),
-  ('61031', '08:30', '18:30', 3798000197, 'roma@sede2.it'),
-  ('41261', '08:30', '19:00', 0863479810, 'math@sede3.com');
+  ('61040', 'Firenze', '14',  'Viale Alessandro Guidoni'),
+  ('61031', 'Roma',    '9',   'Via Cristian Cocco'),
+  ('41261', 'Milano',  '1',   'Via Fieno');
 
 insert into residenzaCliente(cliente,città,civico,via) values
   ('AP1017Y890', 'Roma',               '2',   'Via Ugo Bertossi')
@@ -150,20 +146,20 @@ insert into CasaAutomobilistica(nome,nazione) values
   ('Fiat', 'Italia'),
   ('Mercedes', 'Germania'),
   ('Peugeot', 'Francia'),
-  ('Renault', 'Francia');
-  ('AlfaRomeo', 'Italia')
+  ('Renault', 'Francia'),
+  ('AlfaRomeo', 'Italia');
 
 insert into AutovetturaNoleggiabile(targa,immatricolazione,km,colore,disponibile,carGroup,casaAuto) values
-  ('FR249GG', '2019-12-09', 30000,  'rosso',     false, '',  'AlfaRomeo'),
-  ('FP445PM', '2020-01-11', 10000,  'grigio',    true,  '',  'Audi'),
+  ('FR249GG', '2019-12-09', 30000,  'rosso',     false, 'E',  'Audi'),
+  ('FP445PM', '2020-01-11', 10000,  'grigio',    true,  'A',  'Renault'),
   ('FZ142PG', '2020-02-04', 7000,   'arancione', true,  'L', 'Mercedes'),
-  ('FA101MM', '2019-01-20', 120000, 'nero',      true,  '',  'BMW'),
-  ('FC552KM', '2019-03-10', 100000, 'blu',       false, '',  'Renault'),
-  ('FK597FM', '2019-07-30', 89000,  'bianco',    true,  '',  'Peugeot'),
-  ('FQ647JK', '2019-08-19', 50000,  'rosso',     true,  '',  'Audi'),
-  ('FW830IO', '2019-09-22', 60000,  'nero',      false, '',  'AlfaRomeo'),
-  ('FB392TT', '2019-01-05', 120000, 'grigio',    false, '',  'Fiat'),
-  --('FF483AB', '2019-03-16', 90000,  'blu',       true,  '',  'Renault'),
+  ('FA101MM', '2019-01-20', 120000, 'nero',      true,  'A',  'BMW'),
+  ('FC552KM', '2019-03-10', 100000, 'blu',       false, 'C',  'Renault'),
+  ('FK597FM', '2019-07-30', 89000,  'bianco',    true,  'G',  'Peugeot'),
+  ('FQ647JK', '2019-08-19', 50000,  'rosso',     true,  'L',  'Audi'),
+  ('FW830IO', '2019-09-22', 60000,  'nero',      false, 'D',  'AlfaRomeo'),
+  ('FB392TT', '2019-01-05', 120000, 'grigio',    false, 'A',  'Fiat'),
+  ('FF483AB', '2019-03-16', 90000,  'blu',       true,  'H',  'BMW'),
   --('FI994CD', '2019-07-02', 50000,  'blu',       true,  '',  'Peugeot'),
   --('FX277DD', '2020-03-23', 4000,   'grigio',    true,  '',  'Mercedes'),
   --('FD295RO', '2019-05-30', 87000,  'viola',     false, '',  'BMW'),
@@ -176,16 +172,16 @@ insert into AutovetturaNoleggiabile(targa,immatricolazione,km,colore,disponibile
 
 
 insert into AutovetturaVendita(targa,immatricolazione,km,colore,prezzoVendita,carGroup,casaAuto) values
-  ('ER245GG', '2016-12-09', 150000, 'rosso',     32000, '',  'AlfaRomeo'),
-  ('EP443PM', '2017-01-11', 150000, 'grigio',    41000, '',  'Audi'),
+  ('ER245GG', '2016-12-09', 150000, 'rosso',     32000, 'E',  'AlfaRomeo'),
+  ('EP443PM', '2017-01-11', 150000, 'grigio',    41000, 'J',  'Audi'),
   ('EZ132PG', '2017-02-04', 150000, 'arancione', 40000, 'L', 'Mercedes'),
-  ('EA541MM', '2017-01-20', 150000, 'nero',      2100,  '',  'BMW'),
-  ('EC642KM', '2017-03-10', 150000, 'blu',       8000,  '',  'Renault'),
-  ('EK647FM', '2017-07-30', 150000, 'bianco',    7000,  '',  'Peugeot'),
-  ('EQ227JK', '2017-08-19', 150000, 'rosso',     12000, '',  'Audi'),
-  ('EW520IO', '2017-09-22', 150000, 'nero',      22000, '',  'AlfaRomeo'),
-  ('EB122TT', '2017-01-05', 150000, 'grigio',    10000, '',  'BMW'),
-  ('EF813AB', '2017-03-16', 150000, 'blu',       5000,  '',  'Renault'),
+  ('EA541MM', '2017-01-20', 150000, 'nero',      2100,  'P',  'BMW'),
+  ('EC642KM', '2017-03-10', 150000, 'blu',       8000,  'A',  'Renault'),
+  ('EK647FM', '2017-07-30', 150000, 'bianco',    7000,  'C',  'Peugeot'),
+  ('EQ227JK', '2017-08-19', 150000, 'rosso',     12000, 'H',  'Audi'),
+  ('EW520IO', '2017-09-22', 150000, 'nero',      22000, 'A',  'AlfaRomeo'),
+  ('EB122TT', '2017-01-05', 150000, 'grigio',    10000, 'C',  'BMW'),
+  ('EF813AB', '2017-03-16', 150000, 'blu',       5000,  'L',  'Renault'),
   --('EI774CD', '2017-07-02', 150000, 'blu',       5500,  '',  'Peugeot'),
   --('EX137DD', '2017-03-23', 150000, 'grigio',    29000, '',  'Mercedes'),
   --('ED635RO', '2017-05-30', 150000, 'viola',     18000, '',  'BMW'),
@@ -205,7 +201,7 @@ insert into AcquirenteVetturaUsata (nomeAzienda,partitaIva,telefono,datiBancari)
 
 --4 indirizzi 4 dati bancari
 insert into Fornitore (nomeAziendaFornitore,partitaIva,telefono,datiBancari) values
-  ()
+  ('Perinetti auto', '12034810019')
 
 insert into sedeAttuale (autovetturaN, sede) values
   ('FR249GG', '61040'),
@@ -218,8 +214,6 @@ insert into sedeAttuale (autovetturaN, sede) values
   ('FW830IO', '41261'),
   ('FB392TT', '41261'),
 
---non ho controllato i car group
---DA CORREGGERE
 insert into noleggioAutovetturaNoleggiabile(contratto,autovetturaN) values
   (00001, 'FR249GG'), --in corso
   (00002, 'FB392TT'), --in corso
@@ -229,29 +223,48 @@ insert into noleggioAutovetturaNoleggiabile(contratto,autovetturaN) values
   (00006, 'FK597FM'),
   (00007, 'FQ647JK');
 
-  --non ho controllato i car group
-  --DA CORREGGERE
 insert into noleggioAutovetturaVendita(contratto,autovetturaV) values
   (00008, 'EQ227JK'),
   (00009, 'EP443PM'),
   (00010, 'EC642KM'),
   (00011, 'EK647FM'),
-  (00012, 'EF813AB'),
+  (00012, 'EF813AB'); L
 
 --2 indirizzi
 insert into Officina(nome, orarioApertura, orarioChiusura, numTel, email) values
+  ('Milli srl',       '09:00', '18:00', 065699991, 'millisrl@gmail.com'),
+  ('Ncm carrozzeria', '09:30', '19:00', 025198713, 'ncmcarr@outlook.com');
 
 insert into RiparazioniEffettuate(numeroRip,data,motivazione,costo,officina) values
+  (0001, '2019-04-09', 'cambio olio motore e cambio filtro ant.', 80.00,  'Milli srl'),
+  (0002, '2019-06-18', 'cambio paraurti per incidente',           100.00, 'Milli srl'),
+  (0003, '2020-01-14', 'cambio ruote',                            120.00, 'Ncm carrozzeria'),
+  (0004, '2020-04-18', 'rodaggio e controllo freni',              50.00,  'Milli srl'),
+  (0005, '2020-07-22', 'controllo freni e cambio gomme',          100.00, 'Ncm carrozzeria'),
+  (0006, '2020-02-28', 'cambio fari e luci di posizione',         40.00,  'Milli srl'),
+  (0007, '2020-12-25', 'riparazione cerchione',                   20.00,  'Ncm carrozzeria'),
+  (0008, '2020-03-24', 'cambio olio',                             15.00,  'Milli srl'),
+  (0009, '2020-04-01', 'riparazione marmitta',                    30.00,  'Milli srl'),
+  (0010, '2020-05-06', 'riverniciatura paraurti e cambio pomello', 70.00, 'Ncm carrozzeria');
 
 insert into riparazioneAutovetturaN(riparazione,autovetturaN) values
+  ()
 
 insert into riparazioneAutovetturaV(riparazione,autovetturaV) values
 
 insert into indirizzoOfficina(officina,città,civico,via) values
+  ('Milli srl', 'Milano', '17',  'Piazza del Duomo')
+  ('Ncm carrozzeria', 'Roma',   '113', 'Via Salaria')
 
 insert into FatturaVendita(numeroFattura,importo,data,autovettura,acquirente) values
 
 insert into FatturaAcquisto(numeroFattura,importo,data,numVetture,fornitore) values
+  (00028, 20000.00, '2017-03-24', 3, 'Il mondo delle auto'),
+  (00029, 100000.00,'2017-04-01', 6, 'Perinetti auto'),
+  (00031, 24000.00, '2018-12-14', 2, 'Il mondo delle auto'),
+  (00032, 6000.00,  '2018-01-12', 1, 'Perinetti auto'),
+  (00033, 10000.00, '2018-11-07', 4, 'Great cars'),
+  (00034, 70000.00, '2018-09-19', 3, 'Perinetti auto');
 
 insert into acquistoAutovetturaNoleggiabili(fatturaN,autovetturaN) values
 
@@ -264,7 +277,7 @@ insert into fornitoreCasa(fornitore,casa) values
 insert into indirizzoFornitore(fornitore,città,civico,via) values
 
 insert into indirizzoAcquirente(acquirente,città,civico,via) values
-  ('comprousato.srl', 'Pisa',               '22',  'Via della Torre'),
-  ('autoricambi',     'Roma',               '1',   'Via Tiburtina'),
-  ('centoallora.com', 'Genova',             '14',  'Via degli Abeti'),
-  ('tuttomotori',     'Bari',               '9',   'Via Cristian Cocco');
+  ('comprousato.srl', 'Pisa',   '22',  'Via della Torre'),
+  ('autoricambi',     'Roma',   '1',   'Via Tiburtina'),
+  ('centoallora.com', 'Genova', '14',  'Via degli Abeti'),
+  ('tuttomotori',     'Bari',   '9',   'Via Cristian Cocco');
