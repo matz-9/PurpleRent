926873b00bc19f48ffcf26029c30ff6447c483c3
Kiluah <igor.provornyy@gmail.com>
2020-05-27 16:37:44 +0200

    ....

--- purpleRent_queries.sql
+++ purpleRent_queries.sql
@@ -1,10 +1,10 @@
 select nome,cognome,numDocumento,età,datiBancari,sedeRitiro,SedeRilascio
-  from prenotazione,clienteNoleggio
-    where prenotazione.numeroPrenotazione=7003
-      and prenotazione.cliente=clienteNoleggio.numDocumento;
+from prenotazione,clienteNoleggio
+where prenotazione.numeroPrenotazione=7003
+    and prenotazione.cliente=clienteNoleggio.numDocumento;
 
 select targa, sede, casaAuto ,km
-  from AutovetturaNoleggiabile as an, Sede, sedeAttuale
-    where an.targa = sedeAttuale.autovetturaN
-      and sedeAttuale.sede = Sede.codiceMnemonico
-        and an.disponibile = true;
+from AutovetturaNoleggiabile as vettura, Sede, sedeAttuale
+where vettura.targa = sedeAttuale.autovetturaN
+    and sedeAttuale.sede = Sede.codiceMnemonico
+    and vettura.disponibile = true;
