062b576740df43c6e796b0d948b216f2324201c7
Kiluah <igor.provornyy@gmail.com>
2020-05-27 12:57:02 +0200

    ..

--- purpleRent_queries.sql
+++ purpleRent_queries.sql
@@ -1,1 +1,4 @@
-s
+select sedeRitiro,SedeRilascio,nome,cognome,numDocumento,età,datiBancari
+  from prenotazione,clienteNoleggio
+    where prenotazione.numeroPrenotazione=7003
+      and prenotazione.cliente=clienteNoleggio.numDocumento;
